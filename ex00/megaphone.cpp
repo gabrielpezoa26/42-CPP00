5b38c95b92ec2e4b04f59fc8b5a9b20433fd4ab1
gcesar-n <gcesar-n@student.42.ft>
2025-07-09 16:58:52 -0300

    -

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 15:02:22 by gcesar-n          #+#    #+#             */
+/*   Updated: 2025/07/09 16:58:27 by gcesar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	char	*input;
+	
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
+		return (0);
+	}
+	// else if (argv[1])
+	// {
+	// 	input = argv[1];
+	// 	while(argv[1])
+	// 	{
+	// 		std::cout << input;
+	// 	}
+	// }
+	return (0);
+}
