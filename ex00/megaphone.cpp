429ee6ebb6d76b10baa18b640ce7ff8b543e21b7
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-08-27 09:37:13 -0300

    -

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 15:02:22 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/07/09 16:58:27 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/08/27 09:35:42 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 int main(int argc, char **argv)
 {
 	char	*input;
-	
+	int	i = 0;
+	int	count = 0;
+
+	input = argv[1];
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
 		return (0);
 	}
-	// else if (argv[1])
-	// {
-	// 	input = argv[1];
-	// 	while(argv[1])
-	// 	{
-	// 		std::cout << input;
-	// 	}
-	// }
+	else if (argc > 1)
+	{
+		while(input[i] != '\0' && count < (argc - 1))
+		{
+			if (input[i] == ' ')
+				count++;
+			std::cout << (char)toupper(input[i]);
+			i++;
+		}
+	}
 	return (0);
 }
