e7a0d261033a7860bf80f6f2c69a02bf1fb9588d
gcesar-n <gcesar-n@student.42.ft>
2025-09-01 13:06:13 -0300

    splited main funcion

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,33 +6,35 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 15:02:22 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/08/27 09:35:42 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/09/01 13:03:58 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv)
+static void megaphone(char *input)
 {
-	char	*input;
 	int	i = 0;
 	int	count = 0;
 
-	input = argv[1];
+	while(input[i] != '\0')
+	{
+		if (input[i] == ' ')
+			count++;
+		std::cout << (char)toupper(input[i]);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	char	*input = argv[1];
+
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
 		return (0);
 	}
-	else if (argc > 1)
-	{
-		while(input[i] != '\0' && count < (argc - 1))
-		{
-			if (input[i] == ' ')
-				count++;
-			std::cout << (char)toupper(input[i]);
-			i++;
-		}
-	}
+	megaphone(input);
 	return (0);
 }
