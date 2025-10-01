4089544599390ed8685499ab90fb97cc5a41fd26
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-09-30 21:01:13 -0300

    start ex01

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,33 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:26:09 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/09/01 13:26:09 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/09/30 12:29:07 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../phonebook.hpp"
+
+static bool add_contact();
+static bool search_contact();
+static bool quit_phonebook();
+
+static bool add_contact()
+{
+	
+}
+
+static bool search_contact()
+{
+	
+}
+
+static bool quit_phonebook()
+{
+	
+}
+
+int main(int argc, char **argv)
+{
+
+	
+}
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,7 +6,37 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:26:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/09/01 13:26:07 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/09/29 17:54:52 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	private:
+
+	std::string Name;
+	std::string SurName;
+	std::string FaxNumber;
+	std::string PhoneNumber;
+	
+	public:
+	
+};
+
+class Phonebook
+{
+	private:
+	Contact contact_list[8];
+	
+	public:
+
+	
+};
+
+#endif
