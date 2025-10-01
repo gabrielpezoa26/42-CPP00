3110420487494564147f6d6639be8f994477da73
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-10-01 15:36:30 -0300

    -

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,33 +6,32 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:26:09 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/09/30 12:29:07 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:00:15 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../phonebook.hpp"
+#include "phonebook.hpp"
 
 static bool add_contact();
 static bool search_contact();
 static bool quit_phonebook();
 
-static bool add_contact()
-{
-	
-}
-
-static bool search_contact()
-{
-	
-}
-
-static bool quit_phonebook()
-{
-	
-}
-
 int main(int argc, char **argv)
 {
+	std::string command;
 
-	
+	command = argv[1];
+	if(!command)
+		return 1;  //printar erro
+
+	while (1)
+	{
+		if (command == "ADD")
+			add_contact();
+		if (command == "SEARCH")
+			search_contact();
+		if (command == "EXIT")
+			quit_phonebook();
+	}
+	return (0);
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,24 +6,23 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:26:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/09/29 17:54:52 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:20:17 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
-
 #include <iostream>
 #include <string>
 
 class Contact
 {
 	private:
-
-	std::string Name;
-	std::string SurName;
-	std::string FaxNumber;
+	std::string FirstName;
+	std::string LastName;
+	std::string Nickname;
 	std::string PhoneNumber;
+	std::string DarkestSecret;
 	
 	public:
 	
@@ -35,6 +34,7 @@ class Phonebook
 	Contact contact_list[8];
 	
 	public:
+	void add_contact();
 
 	
 };
