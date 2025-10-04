34a3c39d9682739cc394a601f5870767b243f544
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-10-04 14:20:53 -0300

    refactored ADD && empty validation

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -6,40 +6,101 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:26:09 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/03 18:42:35 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/04 14:18:10 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
 
+void Phonebook::promptFirstName(std::string user_input)
+{
+	while(true)
+	{
+		std::cout << "Please enter the first name: ";
+		getline(std::cin, user_input);
+		if (!user_input.empty())
+		{
+			std::cout << "DEBUG: boaa\n";
+			break ;
+		}
+	}
+	contact_list[contact_index].setFirstName(user_input);
+	std::cout << "DEBUG: first name = " << user_input << std::endl;
+}
+
+void Phonebook::promptLastName(std::string user_input)
+{
+	while(true)
+	{	
+		std::cout << "Please enter the last name: ";
+		getline(std::cin, user_input);
+		if (user_input.empty())
+		{
+			std::cout << "DEBUG: boaa\n";
+			break ;
+		}
+	}
+	contact_list[contact_index].setLastName(user_input);
+	std::cout << "DEBUG: last name = " << user_input << std::endl;
+}
+
+void Phonebook::promptNickName(std::string user_input)
+{
+	while(true)
+	{	
+		std::cout << "Please enter the nickname: ";
+		getline(std::cin, user_input);
+		if (user_input.empty())
+		{
+			std::cout << "DEBUG: boaa\n";
+			break ;
+		}
+	}
+	contact_list[contact_index].setNickName(user_input);
+	std::cout << "DEBUG: nickname = " << user_input << std::endl;
+}
+
+void Phonebook::promptPhoneNumber(std::string user_input)
+{
+	while(true)
+	{	
+		std::cout << "Please enter the phonenumber: ";
+		getline(std::cin, user_input);
+		if (user_input.empty())
+		{
+			std::cout << "DEBUG: vishhhh\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+		contact_list[contact_index].setPhoneNumber(user_input);
+		std::cout << "DEBUG: phonenumber = " << user_input << std::endl;
+}
+
+void Phonebook::promptDarkestSecret(std::string user_input)
+{
+	while(true)
+	{
+		std::cout << "Please enter the darkest secret: ";
+		getline(std::cin, user_input);
+		if (user_input.empty())
+		{
+			std::cout << "DEBUG: vishhhh\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	contact_list[contact_index].setDarkestSecret(user_input);
+	std::cout << "DEBUG: secret = " << user_input << std::endl;
+}
+
 void Phonebook::addContact()
 {
 	std::string user_input;
 
-	std::cout << "Please enter the first name: ";
-	getline(std::cin, user_input);
-	contact_list[contact_index].setFirstName(user_input);
-	std::cout << "DEBUG: first name = " << user_input << std::endl;
-
-	std::cout << "Please enter the last name: ";
-	getline(std::cin, user_input);
-	contact_list[contact_index].setLastName(user_input);
-	std::cout << "DEBUG: last name = " << user_input << std::endl;
-
-	std::cout << "Please enter the nickname: ";
-	getline(std::cin, user_input);
-	contact_list[contact_index].setNickName(user_input);
-	std::cout << "DEBUG: nickname = " << user_input << std::endl;
-
-	std::cout << "Please enter the phonenumber: ";
-	getline(std::cin, user_input);
-	contact_list[contact_index].setPhoneNumber(user_input);
-	std::cout << "DEBUG: phonenumber = " << user_input << std::endl;
-	
-	std::cout << "Please enter the darkest secret: ";
-	getline(std::cin, user_input);
-	contact_list[contact_index].setDarkestSecret(user_input);
-	std::cout << "DEBUG: secret = " << user_input << std::endl;
+	promptFirstName(user_input);
+	promptLastName(user_input);
+	promptNickName(user_input);
+	promptPhoneNumber(user_input);
+	promptDarkestSecret(user_input);
 
 	std::cout << "Contact added successfully!" << std::endl;
 	contact_index++;
--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 20:00:11 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/03 18:08:43 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/04 14:12:27 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,16 @@ class Phonebook
 	private:
 	Contact contact_list[8];
 	int		contact_index;
+	void promptFirstName(std::string user_input);
+	void promptLastName(std::string user_input);
+	void promptNickName(std::string user_input);
+	void promptPhoneNumber(std::string user_input);
+	void promptDarkestSecret(std::string user_input);
 
 	public:
 		Phonebook()
 		{
 			contact_index = 0;
-			std::cout << "hellooo";
 		}
 	void addContact();
 	void searchContact();
